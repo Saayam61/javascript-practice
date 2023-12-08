06d72ef3d80f5159f8be11950ff42716ee70be3f
Saayam61 <97223984+Saayam61@users.noreply.github.com>
2023-12-08 14:31:55 +0545

    Add files via upload

---
+++ assign.js
@@ -0,0 +1,19 @@
+
+    var number = 10;
+
+    var result1 = number;
+    console.log("Result 1 (assignment):", result1);
+    number += 10;
+    var result2 = number;
+    console.log("Result 2 (addition assignment):", result2);
+    number -= 5;
+    var result3 = number;
+    console.log("Result 3 (subtraction assignment):", result3);
+    number *= 3;
+    var result4 = number;
+    console.log("Result 4 (multiplication assignment):", result4);
+    number /= 2;
+    var result5 = number;
+    console.log("Result 5 (division assignment):", result5);
+
+
---
+++ incdec.js
@@ -0,0 +1,5 @@
+var num =5;
+num++;
+console.log("After increment, the number is ",num);
+num--;
+console.log("After decrement, the number is ",num);
